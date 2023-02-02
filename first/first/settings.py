8da7fc474fe22b16e254ed74dfe2a62d4ed7655b
[ishkining] <ishkining@gmail.com>
2023-02-02 12:11:09 +0500

    add some fixers

--- first/first/settings.py
+++ first/first/settings.py
@@ -20,11 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
+
 SECRET_KEY =
-=======
-SECRET_KEY = #UR_SECRET_KEY
->>>>>>> 1f7a75d2783608cce1a4c48da517601ba6e83978
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
