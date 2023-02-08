088e3610fd58c7780abd722ce088caebcecfcee2
[ishkining] <ishkining@gmail.com>
2023-02-08 13:59:20 +0500

    add eighth hour of learning django and sql learning

--- movie_proj/mive_app/models.py
+++ movie_proj/mive_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=100000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}'
--- movie_proj/mive_app/views.py
+++ movie_proj/mive_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'mive_app/all_movies.html', context= {
+        'movies': movies,
+    })
